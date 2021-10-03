c95937480f098041b9732945b7d67a13e1d2956e
Sudhakar Jha <5928299+SudhakarJha@users.noreply.github.com>
2021-10-03 16:58:32 +1300

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Cloning dependencies"
-repo init -u git://github.com/DotOS/manifest.git -b dot11
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-source build/envsetup.sh
-lunch dot_walleye-userdebug
-make bacon
+repo init -u git://github.com/DotOS/manifest.git -b dot11  &> /dev/null
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags  &> /dev/null
+source build/envsetup.sh  &> /dev/null
+lunch dot_walleye-userdebug  &> /dev/null
+make bacon  &> /dev/null
